4f020948775639157a9e0b38fff580a0ff985279
Lukas Lansky <lukas.lansky@suse.com>
2022-01-09 16:53:32 +0100

    Almost full implementation of the complement. Restart function is however just a placeholder.

--- cplmnt/safram_cplmnt.h
+++ cplmnt/safram_cplmnt.h
@@ -1,9 +1,14 @@
+#ifndef SAFRAM_CPLMNT_H
+#define SAFRAM_CPLMNT_H
 
-/*
-#define NUMARGS(...)  (sizeof((int[]){__VA_ARGS__})/sizeof(int))
+/* Macros for UINT8 arrays
+   - single variable or other types could be covered with similar macros
 */
 
-#define FCN_UINT8_CPLMNT(_value) (uint8_t) ~(_value)
+#define SAFRAM_DEF_UINT8_ARRAY(_name) __attribute__((section(".data_safram"))) uint8_t _name[]
+#define SAFRAM_DEF_UINT8_ARRAY_CPLMNT(_name) __attribute__((section(".data_safram_cplmnt"))) uint8_t _name##_cplmnt[]
+
+#define FCN_UINT8_CPLMNT(_value) ((uint8_t) ~(_value))
 
 #define SAFRAM_SET_UINT8_ARRAY(_name, _idx, _value) \
     do { \
@@ -12,13 +17,26 @@
     } while(0)
 
 #define SAFRAM_GET_UINT8_ARRAY(_name, _idx, _failcall) \
-        ((_name[_idx]) == (uint8_t) ~(_name##_cplmnt[_idx])) ? ( _name[_idx]) : \
-	    (_failcall(), (_name[_idx]))
+        ((_name[_idx]) == FCN_UINT8_CPLMNT(_name##_cplmnt[_idx])) ? ( _name[_idx]) : \
+	    (_failcall, (_name[_idx]))
 
-/*
-#define SAFRAM_DEF_UINT8_ARRAY(_name, _value) \
-    do { \
-        __attribute__((section(".data_safram"))) uint8_t _name[] = _value; \
-        __attribute__((section(".data_safram_cplmnt"))) uint8_t _name##_cplmnt[_size]; \        
-    } while(0)
+/*  Memory addresses for both sections of safety RAM 
+    - these will be supplied during linking
+    - these are not required for protection and checking really 
 */
+
+// Memory section where the variables are stored
+extern uint32_t _DATA_SAFRAM_START;
+extern uint32_t _DATA_SAFRAM_END;
+
+#define PTR_UINT8_SAFRAM_START (uint8_t*)&_DATA_SAFRAM_START
+#define PTR_UINT8_SAFRAM_END   (uint8_t*)&_DATA_SAFRAM_END 
+
+// Memory section where the variables one's complements are stored
+extern uint32_t _DATA_SAFRAM_CPLMNT_START;
+extern uint32_t _DATA_SAFRAM_CPLMNT_END;
+
+#define PTR_UINT8_SAFRAM_CPLMNT_START (uint8_t*)&_DATA_SAFRAM_CPLMNT_START
+#define PTR_UINT8_SAFRAM_CPLMNT_END   (uint8_t*)&_DATA_SAFRAM_CPLMNT_END 
+
+#endif /* SAFRAM_CPLMNT_H */
--- cplmnt/th_safram_cplmnt.c
+++ cplmnt/th_safram_cplmnt.c
@@ -1,29 +1,54 @@
 #include <stdint.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "safram_cplmnt.h"
 
-void restart()
+void restart(uint8_t x)
 {
-	printf("This is restart!\n");
+	printf("This is restart %u!\n", x);
+}
+
+SAFRAM_DEF_UINT8_ARRAY(testarray) = {255, 254, 253, 252, 251, 250, 249, 248};
+SAFRAM_DEF_UINT8_ARRAY_CPLMNT(testarray) = {0, 1, 2, 3, 4, 5, 6, 7};
+
+void periodic_task(char **argv, uint8_t count)
+{
+	/* No specific safety critical section */
+    uint8_t value;
+    for(uint8_t i = 0; i < 8; i++) {
+        value = SAFRAM_GET_UINT8_ARRAY(testarray, i, restart(i));
+        if (i == 1) {
+            printf("testarray[%u]=0x%02X, one's complement=0x%02X\n", i, value, FCN_UINT8_CPLMNT(value));
+            value += i;
+            SAFRAM_SET_UINT8_ARRAY(testarray, i, value);
+        }
+    }
+	sleep(1);
 }
 
 int main(int argc, char* argv[]) 
 {
-    if (argc != 1) {
+    uint8_t count = argc > 1 ? atoi(argv[1]) : 0;
+    uint8_t *safram_start = PTR_UINT8_SAFRAM_START;
+    uint8_t *safram_cplmnt_start = PTR_UINT8_SAFRAM_CPLMNT_START;
+	uint8_t *safram_end = PTR_UINT8_SAFRAM_END;
+	uint8_t *safram_cplmnt_end = PTR_UINT8_SAFRAM_CPLMNT_END; 
+    
+    if (argc > 2) {
         printf("Usage:\n");
-        printf("  th_safram_cplmnt\n");
+        printf("  th_safram_cplmnt count\n");
         exit(1);
     }
-    uint8_t testarray[] = {0,0,0,0};
-    uint8_t testarray_cplmnt[] = {0,0,0,0};
-    
-    SAFRAM_SET_UINT8_ARRAY(testarray, 0, 0x55);
 
-    printf("testarray[0]=0x%X, testarray_cpl[0]=0x%X\n", testarray[0],testarray_cplmnt[0]);
-    
-    printf("SAFRAM_GET_UINT8_ARRAY(testarray, 0)=0x%X\n", SAFRAM_GET_UINT8_ARRAY(testarray, 0, restart));
-    testarray_cplmnt[0] = 0;
-    printf("SAFRAM_GET_UINT8_ARRAY(testarray, 0)=0x%X\n", SAFRAM_GET_UINT8_ARRAY(testarray, 0, restart));
+    printf("This is an example of protecting RAM variable with one's complement! Run #%d!\n", count);
+	printf("pid=%d\n", getpid());
+    printf(".data_safram       , start_address=%p, length=%lu\n", safram_start, safram_end - safram_start);
+    printf(".data_safram_cplmnt, start_address=%p, length=%lu\n", safram_cplmnt_start, safram_cplmnt_end - safram_cplmnt_start);
+	
+    /* Executing periodic task and waiting for external memory corruption */
+	while(1) {
+		periodic_task(argv, count);
+	}
 }
